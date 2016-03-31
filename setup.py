f7b87f4f295ef48c09f67b5de45ddeb1b7766093
Zach Adams <zach@okzach.com>
2016-03-31 03:14:49 -0500

    bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='bofhexcuse',
-    version='0.2.0',
+    version='0.3.0',
     packages=['bofhexcuse'],
     url='https://github.com/okzach/bofhexcuse/',
     license='',
